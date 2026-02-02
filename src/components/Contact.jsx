86ecb91bef2dd14c91bfc05296784650c38b0b9f
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2026-02-02 20:17:07 +0530

    contact btn liks fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,22 +9,26 @@ export default function Contact(){
         
         <p>Email: <a className='mailLink' href="mailto:iamsingh.hj@gmail.com">iamsingh.hj@gmail.com</a></p>
         <div className='contact-btns'>
-          <button className='contact-btn'>
-            <Linkedin color='#1B4B4B' strokeWidth={1}/> 
-            <a href='https://www.linkedin.com/in/codeharman/' target='_blank'>LinkedIn</a>
-          </button>
-          <button className='contact-btn'>
-            <Github color='#1B4B4B' strokeWidth={1}/>
-            <a href='https://github.com/codeharman' target='_blank'>GitHub</a>
-          </button>
-          <button className='contact-btn'>
-            <Twitter color='#1B4B4B' strokeWidth={1} />
-            <a href='https://x.com/codeharmann' target='_blank'>Twitter / X</a>
-          </button>
-           <button className='contact-btn'>
-            <Youtube color='#1B4B4B' strokeWidth={1} />
-            <a href='https://www.youtube.com/@codeharman' target='_blank'>YouTube</a>
-          </button>
+          <a href='https://www.linkedin.com/in/codeharman/' target='_blank'>
+            <button className='contact-btn'>
+              <Linkedin color='#1B4B4B' strokeWidth={1}/> LinkedIn
+            </button>
+          </a>
+          <a href='https://github.com/codeharman' target='_blank'>
+            <button className='contact-btn'>
+              <Github color='#1B4B4B' strokeWidth={1}/> GitHub
+            </button>
+          </a>
+          <a href='https://x.com/codeharmann' target='_blank'>
+            <button className='contact-btn'>
+              <Twitter color='#1B4B4B' strokeWidth={1} /> Twitter / X
+            </button>
+          </a>
+          <a href='https://www.youtube.com/@codeharman' target='_blank'>
+            <button className='contact-btn'>
+              <Youtube color='#1B4B4B' strokeWidth={1} />YouTube
+            </button>
+          </a>
         </div>
       </div>
     </div>
